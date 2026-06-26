27cead0e7b250af334d631f446115155071ed5fd
Mapo270322 <polisyahrul1363@gmail.com>
2026-06-26 17:24:41 +0800

    Update service-wolker.js

--- service-wolker.js
+++ service-wolker.js
@@ -1,1 +1,22 @@
 
+const CACHE_NAME = "mapo-v1";
+
+const urlsToCache = [
+  "/MAPO-Alfath-Maret/",
+  "/MAPO-Alfath-Maret/index.html",
+  "/MAPO-Alfath-Maret/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
