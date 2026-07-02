85d030d62dad56cb61f99d721034b2f88cfc0ef7
Mapo270322 <polisyahrul1363@gmail.com>
2026-07-02 19:37:12 +0800

    Create produk.js

---
+++ produk.js
@@ -0,0 +1,30 @@
+const produk = [
+  {
+    nama: "TELUR AYAM 10 btr",
+    harga: 29000,
+    kategori: "Telur",
+    gambar: "telur.jpg",
+    bestSeller: true
+  },
+  {
+    nama: "ISTANA PANGAN 5 kg",
+    harga: 84000,
+    kategori: "Beras",
+    gambar: "istana.jpg",
+    bestSeller: true
+  },
+  {
+    nama: "DUA MERPATI",
+    harga: 83999,
+    kategori: "Beras",
+    gambar: "duamerpati.jpg",
+    bestSeller: false
+  },
+  {
+    nama: "BIMOLI 2L",
+    harga: 48500,
+    kategori: "Minyak Goreng",
+    gambar: "bimoli.jpg",
+    bestSeller: true
+  }
+];
