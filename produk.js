4368ce0bd64fc87028a70445f4d44227183ba506
Mapo270322 <polisyahrul1363@gmail.com>
2026-07-04 18:34:23 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -3,28 +3,28 @@ const produk = [
     nama: "TELUR AYAM 10 btr",
     harga: 29000,
     kategori: "Telur",
-    gambar: "telurayam.jpg",
+    gambar: "https://mapo270322.github.io/MAPO-Alfath-Maret/telurayam.jpg"
     bestSeller: true
   },
   {
     nama: "ISTANA PANGAN 5 kg",
     harga: 84000,
     kategori: "Beras",
-    gambar: "istanapangan.jpg",
+    gambar: "https://mapo270322.github.io/MAPO-Alfath-Maret/istanapangan.jpg"
     bestSeller: true
   },
   {
     nama: "DUA MERPATI",
     harga: 83999,
     kategori: "Beras",
-    gambar: "beras.jpg",
+    gambar: "https://mapo270322.github.io/MAPO-Alfath-Maret/beras.jpg"
     bestSeller: false
   },
   {
     nama: "BIMOLI 2L",
     harga: 48500,
     kategori: "Minyak Goreng",
-    gambar: "bimoli.jpg",
+    gambar: "https://mapo270322.github.io/MAPO-Alfath-Maret/bimoli.jpg"
     bestSeller: true
   }
 ];
