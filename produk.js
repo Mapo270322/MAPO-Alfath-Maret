a3588f5739bec6713797e980c36dccf13495ebb6
Mapo270322 <polisyahrul1363@gmail.com>
2026-07-08 15:38:12 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,9 +1,9 @@
 const produk = [
   {
-    nama: "TELUR AYAM 10 btr",
-    harga: 29000,
-    kategori: "Telur",
-    gambar: "telurayam10.jpg",
+    nama: " BERAS SPHP",
+    harga: 62000,
+    kategori: "Beras",
+    gambar: "sphp1.jpg",
     bestSeller: true
   },
   {
@@ -14,24 +14,24 @@ const produk = [
     bestSeller: true
   },
   {
-    nama: "DUA MERPATI 5kg",
+    nama: "DUA MERPATI 5 kg",
     harga: 83999,
     kategori: "Beras",
     gambar: "beras.jpg",
     bestSeller: false
   },
   {
-    nama: "BIMOLI 1 ltr",
-    harga: 24500,
-    kategori: "Minyak Goreng",
-    gambar: "bimoli.jpg",
+    nama: "SUA MERPATI 2 kg",
+    harga: 34000,
+    kategori: "Beras",
+    gambar: "beras.jpg",
     bestSeller: false
     },
   {
-    nama: "INDOMIE",
-    harga: 2500,
-    kategori: "MIE",
-    gambar: "Indomie.jpg",
+    nama: "BERAS BUNAKEN INDAH 5 kg",
+    harga: 62000,
+    kategori: "Beras",
+    gambar: "bunakenindah.jpg",
     bestSeller: false
   },
   {
@@ -39,6 +39,27 @@ const produk = [
     harga: 4600,
     kategori: "MIE",
     gambar: "indomiejumbo.jpg",
+    bestSeller: false
+    },
+    {
+    nama: "TELUR AYAM 10 btr",
+    harga: 29000,
+    kategori: "Telur",
+    gambar: "telurayam10.jpg",
+    bestSeller: true
+    },
+   {
+    nama: "BIMOLI 1 ltr",
+    harga: 24500,
+    kategori: "Minyak Goreng",
+    gambar: "bimoli.jpg",
+    bestSeller: false
+    },
+   {
+    nama: "INDOMIE",
+    harga: 2500,
+    kategori: "MIE",
+    gambar: "Indomie.jpg",
     bestSeller: false
   },
 ];
